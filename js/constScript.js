6bb2a94062036aa7f8db1f4d8f548ff448b342b6
Alejandro Sanguino Osa <116163899+sanguino09@users.noreply.github.com>
2024-08-02 17:06:04 +0200

    Add files via upload

--- js/constScript.js
+++ js/constScript.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.star').forEach(star => {
     star.addEventListener('click', function(e) {
         e.preventDefault();
-        alert('You clicked on ' + this.getAttribute('title'));
+        alert('Vas a ver el cielo de ' + this.getAttribute('title'));
         window.location.href = this.getAttribute('href');
     });
 });
